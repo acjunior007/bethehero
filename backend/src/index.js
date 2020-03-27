97557fd3138682cf38296ea39a3a59e2ae87eb29
Antonio Junior <antonio.junior@fcamara.com.br>
2020-03-27 18:44:44 -0300

    Front end do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 3000;
+const port = 5000;
 const routes = require('./routes');
 
 app.use(cors());
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Header from './Header';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
+  //const [counter, setCounter] = useState(0);
+
+  // function incrementar(){
+  //   setCounter(counter+1);
+  //   //console.log(counter);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Omnistack....
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
+    // <div>
+    // {/* // <Header title="Semana omnistack" /> */}
+    // {/* <Header>Contado: {counter}</Header>
+    // <button onClick={incrementar}>Incrementar</button> */}
+    // </div>
   );
 }
 
